3bc55a36f303372a48b7d6a5ae1add5c1965a731
yerinirey <yerin886@naver.com>
2023-09-07 16:01:32 +0900

    updated

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
@@ -7,7 +8,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
       <h2>
         <Link to={`/movie/${id}`}>{title}/</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
